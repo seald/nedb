0faabdd5b81188bd1cf4a7cbe6bddcb4f252def2
Pierre de la Martiniere <martpie@fb.com>
2022-12-25 19:23:36 +0100

    Format index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@
 
 /// <reference types="node" />
 
-import { EventEmitter } from 'events';
+import { EventEmitter } from "events";
 
 export default Nedb;
 
@@ -18,17 +18,17 @@ declare class Nedb<G = any> extends EventEmitter {
 
   persistence: Nedb.Persistence;
 
-  autoloadPromise: Promise<void>|null;
+  autoloadPromise: Promise<void> | null;
 
   loadDatabase(callback?: (err: Error | null) => void): void;
 
   loadDatabaseAsync(): Promise<void>;
 
-  dropDatabase(callback?: (err: Error |null) => void): void;
+  dropDatabase(callback?: (err: Error | null) => void): void;
 
   dropDatabaseAsync(): Promise<void>;
 
-  compactDatafile(callback?: (err: Error |null) => void): void;
+  compactDatafile(callback?: (err: Error | null) => void): void;
 
   compactDatafileAsync(): Promise<void>;
 
@@ -38,7 +38,10 @@ declare class Nedb<G = any> extends EventEmitter {
 
   getAllData<T extends G>(): T[];
 
-  ensureIndex(options: Nedb.EnsureIndexOptions, callback?: (err: Error | null) => void): void;
+  ensureIndex(
+    options: Nedb.EnsureIndexOptions,
+    callback?: (err: Error | null) => void
+  ): void;
 
   ensureIndexAsync(options: Nedb.EnsureIndexOptions): Promise<void>;
 
@@ -46,8 +49,14 @@ declare class Nedb<G = any> extends EventEmitter {
 
   removeIndexAsync(fieldName: string | string[]): Promise<void>;
 
-  insert<T extends G>(newDoc: T, callback?: (err: Error | null, document: T) => void): void;
-  insert<T extends G>(newDocs: T[], callback?: (err: Error | null, documents: T[]) => void): void;
+  insert<T extends G>(
+    newDoc: T,
+    callback?: (err: Error | null, document: T) => void
+  ): void;
+  insert<T extends G>(
+    newDocs: T[],
+    callback?: (err: Error | null, documents: T[]) => void
+  ): void;
 
   insertAsync<T extends G>(newDoc: T): Promise<T>;
   insertAsync<T extends G>(newDocs: T[]): Promise<T[]>;
@@ -57,36 +66,72 @@ declare class Nedb<G = any> extends EventEmitter {
 
   countAsync(query: any): Nedb.Cursor<number>;
 
-  find<T extends G>(query: any, projection: any, callback?: (err: Error | null, documents: T[]) => void): void;
+  find<T extends G>(
+    query: any,
+    projection: any,
+    callback?: (err: Error | null, documents: T[]) => void
+  ): void;
   find<T extends G>(query: any, projection?: any): Nedb.Cursor<T>;
-  find<T extends G>(query: any, callback: (err: Error | null, documents: T[]) => void): void;
+  find<T extends G>(
+    query: any,
+    callback: (err: Error | null, documents: T[]) => void
+  ): void;
 
   findAsync<T extends G>(query: any, projection?: any): Nedb.Cursor<T[]>;
 
-  findOne<T extends G>(query: any, projection: any, callback: (err: Error | null, document: T) => void): void;
-  findOne<T extends G>(query: any, callback: (err: Error | null, document: T) => void): void;
+  findOne<T extends G>(
+    query: any,
+    projection: any,
+    callback: (err: Error | null, document: T) => void
+  ): void;
+  findOne<T extends G>(
+    query: any,
+    callback: (err: Error | null, document: T) => void
+  ): void;
 
   findOneAsync<T extends G>(query: any, projection?: any): Nedb.Cursor<T>;
 
-  update<T extends G>(query: any, updateQuery: any, options?: Nedb.UpdateOptions, callback?: (err: Error | null, numberOfUpdated: number, affectedDocuments: T | T[] | null, upsert: boolean | null) => void): void;
+  update<T extends G>(
+    query: any,
+    updateQuery: any,
+    options?: Nedb.UpdateOptions,
+    callback?: (
+      err: Error | null,
+      numberOfUpdated: number,
+      affectedDocuments: T | T[] | null,
+      upsert: boolean | null
+    ) => void
+  ): void;
 
-  updateAsync<T extends G>(query: any, updateQuery: any, options?: Nedb.UpdateOptions): Promise<{numAffected: number, affectedDocuments: T|T[]|null, upsert: boolean}>;
+  updateAsync<T extends G>(
+    query: any,
+    updateQuery: any,
+    options?: Nedb.UpdateOptions
+  ): Promise<{
+    numAffected: number;
+    affectedDocuments: T | T[] | null;
+    upsert: boolean;
+  }>;
 
-  remove(query: any, options: Nedb.RemoveOptions, callback?: (err: Error | null, n: number) => void): void;
+  remove(
+    query: any,
+    options: Nedb.RemoveOptions,
+    callback?: (err: Error | null, n: number) => void
+  ): void;
   remove(query: any, callback?: (err: Error | null, n: number) => void): void;
 
   removeAsync(query: any, options: Nedb.RemoveOptions): Promise<number>;
 
-  addListener(event: 'compaction.done', listener: () => void): this;
-  on(event: 'compaction.done', listener: () => void): this;
-  once(event: 'compaction.done', listener: () => void): this;
-  prependListener(event: 'compaction.done', listener: () => void): this;
-  prependOnceListener(event: 'compaction.done', listener: () => void): this;
-  removeListener(event: 'compaction.done', listener: () => void): this;
-  off(event: 'compaction.done', listener: () => void): this;
-  listeners(event: 'compaction.done'): Array<() => void>;
-  rawListeners(event: 'compaction.done'): Array<() => void>;
-  listenerCount(type: 'compaction.done'): number;
+  addListener(event: "compaction.done", listener: () => void): this;
+  on(event: "compaction.done", listener: () => void): this;
+  once(event: "compaction.done", listener: () => void): this;
+  prependListener(event: "compaction.done", listener: () => void): this;
+  prependOnceListener(event: "compaction.done", listener: () => void): this;
+  removeListener(event: "compaction.done", listener: () => void): this;
+  off(event: "compaction.done", listener: () => void): this;
+  listeners(event: "compaction.done"): Array<() => void>;
+  rawListeners(event: "compaction.done"): Array<() => void>;
+  listenerCount(type: "compaction.done"): number;
 }
 
 declare namespace Nedb {
@@ -113,7 +158,7 @@ declare namespace Nedb {
     afterSerialization?(line: string): string;
     corruptAlertThreshold?: number;
     compareStrings?(a: string, b: string): number;
-    modes?: {fileMode: number, dirMode: number};
+    modes?: { fileMode: number; dirMode: number };
     testSerializationHooks?: boolean;
   }
 
