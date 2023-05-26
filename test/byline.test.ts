11118c0dfbf77cb37fdc166f8617fc2abeafa804
Mitch Myburgh <mitchmyburgh@gmail.com>
2023-05-26 21:42:20 +0200

    convert tests

--- test/byline.test.ts
+++ test/byline.test.ts
@@ -19,23 +19,23 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
-const chai = require("chai");
-const fs = require("fs");
-const path = require("path");
-const byline = require("../src/byline");
+import chai from "chai";
+import fs from "fs";
+import path from "path";
+import byline from "../src/byline";
 
 const { assert } = chai;
 
 const regEx = /\r\n|[\n\v\f\r\x85\u2028\u2029]/g;
 const localPath = (file) => path.join(__dirname, "byline", file);
 
-describe("byline", function() {
-  it("should pipe a small file", function(done) {
+describe("byline", function () {
+  it("should pipe a small file", function (done) {
     const input = fs.createReadStream(localPath("empty.txt"));
     const lineStream = byline(input); // convinience API
     const output = fs.createWriteStream(localPath("test.txt"));
     lineStream.pipe(output);
-    output.on("close", function() {
+    output.on("close", function () {
       const out = fs.readFileSync(localPath("test.txt"), "utf8");
       const in_ = fs
         .readFileSync(localPath("empty.txt"), "utf8")
@@ -46,34 +46,34 @@ describe("byline", function() {
     });
   });
 
-  it("should work with streams2 API", function(done) {
+  it("should work with streams2 API", function (done) {
     let stream = fs.createReadStream(localPath("empty.txt"));
     stream = byline(stream);
 
-    stream.on("readable", function() {
+    stream.on("readable", function () {
       while (stream.read() !== null) {
         // eslint-ignore-line no-empty
       }
     });
 
-    stream.on("end", function() {
+    stream.on("end", function () {
       done();
     });
   });
 
-  it("should ignore empty lines by default", function(done) {
+  it("should ignore empty lines by default", function (done) {
     const input = fs.createReadStream(localPath("empty.txt"));
     const lineStream = byline(input);
     lineStream.setEncoding("utf8");
 
     const lines1 = [];
-    lineStream.on("data", function(line) {
+    lineStream.on("data", function (line) {
       lines1.push(line);
     });
 
-    lineStream.on("end", function() {
+    lineStream.on("end", function () {
       let lines2 = fs.readFileSync(localPath("empty.txt"), "utf8").split(regEx);
-      lines2 = lines2.filter(function(line) {
+      lines2 = lines2.filter(function (line) {
         return line.length > 0;
       });
       assert.deepEqual(lines2, lines1);
@@ -81,43 +81,43 @@ describe("byline", function() {
     });
   });
 
-  it("should keep empty lines when keepEmptyLines is true", function(done) {
+  it("should keep empty lines when keepEmptyLines is true", function (done) {
     const input = fs.createReadStream(localPath("empty.txt"));
     const lineStream = byline(input, { keepEmptyLines: true });
     lineStream.setEncoding("utf8");
 
     const lines = [];
-    lineStream.on("data", function(line) {
+    lineStream.on("data", function (line) {
       lines.push(line);
     });
 
-    lineStream.on("end", function() {
+    lineStream.on("end", function () {
       assert.deepEqual(["", "", "", "", "", "Line 6"], lines);
       done();
     });
   });
 
-  it("should not split a CRLF which spans two chunks", function(done) {
+  it("should not split a CRLF which spans two chunks", function (done) {
     const input = fs.createReadStream(localPath("CRLF.txt"));
     const lineStream = byline(input, { keepEmptyLines: true });
     lineStream.setEncoding("utf8");
 
     const lines = [];
-    lineStream.on("data", function(line) {
+    lineStream.on("data", function (line) {
       lines.push(line);
     });
 
-    lineStream.on("end", function() {
+    lineStream.on("end", function () {
       assert.equal(2, lines.length);
       done();
     });
   });
 
-  it("should read a large file", function(done) {
+  it("should read a large file", function (done) {
     readFile(localPath("rfc.txt"), done);
   });
 
-  it("should read a huge file", function(done) {
+  it("should read a huge file", function (done) {
     // Readable highWaterMark is 16384, so we test a file with more lines than this
     readFile(localPath("rfc_huge.txt"), done);
   });
@@ -128,13 +128,13 @@ describe("byline", function() {
     lineStream.setEncoding("utf8");
 
     let lines2 = fs.readFileSync(filename, "utf8").split(regEx);
-    lines2 = lines2.filter(function(line) {
+    lines2 = lines2.filter(function (line) {
       return line.length > 0;
     });
 
     const lines1 = [];
     let i = 0;
-    lineStream.on("data", function(line) {
+    lineStream.on("data", function (line) {
       lines1.push(line);
       if (line !== lines2[i]) {
         console.log("EXPECTED:", lines2[i]);
@@ -144,22 +144,22 @@ describe("byline", function() {
       i++;
     });
 
-    lineStream.on("end", function() {
+    lineStream.on("end", function () {
       assert.equal(lines2.length, lines1.length);
       assert.deepEqual(lines2, lines1);
       done();
     });
   }
 
-  it("should handle encodings like fs", function(done) {
-    areStreamsEqualTypes(undefined, function() {
-      areStreamsEqualTypes({ encoding: "utf8" }, function() {
+  it("should handle encodings like fs", function (done) {
+    areStreamsEqualTypes(undefined, function () {
+      areStreamsEqualTypes({ encoding: "utf8" }, function () {
         done();
       });
     });
   });
 
-  it("should pause() and resume() with a huge file", function(done) {
+  it("should pause() and resume() with a huge file", function (done) {
     const input = fs.createReadStream(localPath("rfc_huge.txt"));
     const lineStream = byline(input);
     lineStream.setEncoding("utf8");
@@ -167,13 +167,13 @@ describe("byline", function() {
     let lines2 = fs
       .readFileSync(localPath("rfc_huge.txt"), "utf8")
       .split(regEx);
-    lines2 = lines2.filter(function(line) {
+    lines2 = lines2.filter(function (line) {
       return line.length > 0;
     });
 
     const lines1 = [];
     let i = 0;
-    lineStream.on("data", function(line) {
+    lineStream.on("data", function (line) {
       lines1.push(line);
       if (line !== lines2[i]) {
         console.log("EXPECTED:", lines2[i]);
@@ -184,12 +184,12 @@ describe("byline", function() {
 
       // pause/resume
       lineStream.pause();
-      setImmediate(function() {
+      setImmediate(function () {
         lineStream.resume();
       });
     });
 
-    lineStream.on("end", function() {
+    lineStream.on("end", function () {
       assert.equal(lines2.length, lines1.length);
       assert.deepEqual(lines2, lines1);
       done();
@@ -201,11 +201,11 @@ describe("byline", function() {
     const lineStream = byline(
       fs.createReadStream(localPath("empty.txt"), options)
     );
-    fsStream.on("data", function(data1) {
-      lineStream.on("data", function(data2) {
+    fsStream.on("data", function (data1) {
+      lineStream.on("data", function (data2) {
         assert.equal(Buffer.isBuffer(data1), Buffer.isBuffer(data2));
       });
-      lineStream.on("end", function() {
+      lineStream.on("end", function () {
         callback();
       });
     });
